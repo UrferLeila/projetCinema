3c17a6bedb37fe7f988e138af8eeb416b6a89d5e
Urfer Leila <leila.urfer@ceff.ch>
2025-12-04 08:07:47 +0100

    PageAcceuil

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,9 @@
+<header>
+    <body class="body">
+        <nav class="navbar">
+            <div>
+                <h1 class="title">Le Palace</h1>
+            </div>
+        </nav>
+    </body>
+</header>
